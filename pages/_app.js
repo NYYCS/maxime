617b83dd964900654a42e4ae8c844dbcc798383e
NYYCS <nyystu@gmail.com>
2021-07-30 19:53:21 +0800

    added material icon font stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,9 @@ import Head from "next/head"
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head></Head>
+      <Head>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet"/>
+      </Head>
       <Component {...pageProps} />
     </>
   )
