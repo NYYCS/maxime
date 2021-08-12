e5182162b0a8980d0faba52931f39790429ac3c9
NYYCS <nyystu@gmail.com>
2021-08-12 18:27:00 +0800

    prototype

--- src/pages/api/hooks.js
+++ src/pages/api/hooks.js
@@ -19,10 +19,10 @@ export const config = {
 
 const stripe = new Stripe("sk_test_51JKweCCK8vBuAGFhY2KkKHTvkc43l1arB2UiA46vLrwmvDOehl4c4ibXhgYnjvI7BMsqLGyl8LoAkjxKDqqMXR8q001GYb69YR");
 
-async function createReservation(reservation, userUuid) {
+async function createReservation(reservation, ) {
   const conn = getDatabase();
   conn.query("INSERT INTO reservations (date, time, guests created_by) VALUES ($1, $2, $3, $4)",
-    [reservation.date, reservation.time, reservation.guests, userUuid])
+    [reservation.date, reservation.time, reservation.guests, reservation.uuid])
 }
 
 export default cors(async function(req, res) {
@@ -34,7 +34,7 @@ export default cors(async function(req, res) {
     if (event.type == "checkout.session.completed") {
       const checkout = await stripe.checkout.sessions.retrieve(event.data.object.id);
       const reservation = checkout.metadata;
-      await createReservation(reservation, userUuid);
+      await createReservation(reservation);
       res.status(200).json({ result: "ok" });
     }
   } catch(error) {
