631cdaf6e3b71972aec7061b2df67deaf09e9e81
NYYCS <nyystu@gmail.com>
2021-08-12 18:10:29 +0800

    test

--- src/pages/api/hooks.js
+++ src/pages/api/hooks.js
@@ -25,20 +25,21 @@ async function createReservation(reservation, userUuid) {
     [reservation.date, reservation.time, reservation.guests, userUuid])
 }
 
-export default cors(async function(req, res) {
-  const sig = req.headers["stripe-signature"];
-  const buf = await getRawBody(req);
-
-  try {
-    const event = stripe.webhooks.constructEvent(buf, sig, WEBHOOK_SECRET);
-    if (event.type == "checkout.session.completed") {
-      const checkout = await stripe.checkout.sessions.retrieve(event.data.object.id);
-      const reservation = checkout.metadata;
-      await createReservation(reservation, userUuid);
-      res.statusCode(200);
-      res.end();
-    }
-  } catch(error) {
-
-  }
-});
+export default async function(req, res) {
+  res.status(200);
+  //const sig = req.headers["stripe-signature"];
+  //const buf = await getRawBody(req);
+//
+  //try {
+  //  const event = stripe.webhooks.constructEvent(buf, sig, WEBHOOK_SECRET);
+  //  if (event.type == "checkout.session.completed") {
+  //    const checkout = await stripe.checkout.sessions.retrieve(event.data.object.id);
+  //    const reservation = checkout.metadata;
+  //    await createReservation(reservation, userUuid);
+  //    res.statusCode(200);
+  //    res.end();
+  //  }
+  //} catch(error) {
+//
+  //}
+};
